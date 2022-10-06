c67b136c361f4aa99c7ebffebd0f0af62591c6b2
TuanKhoi <ledangtuankhoi@gmail.com>
2022-10-06 19:57:59 +0700

    update image for categoru

--- themes/ashion/views/category.blade.php
+++ themes/ashion/views/category.blade.php
@@ -1,6 +1,5 @@
 @php
- $categories = App\Http\Controllers\BlockPageController::BlockCategory();
- dump($categories[0]['name']);
+ $categories = App\Http\Controllers\BlockPageController::BlockCategory(); 
 // error_log($cates);
 @endphp 
 
@@ -11,11 +10,11 @@
         <div class="row">
             <div class="col-lg-6 p-0">
                 <div class="categories__item categories__large__item set-bg"
-                    data-setbg="{{ asset('themes/ashion//img/categories/category-1.jpg') }}">
+                    data-setbg="{{ productImage($categories[0]['image']) }}">
                     <div class="categories__text">
                         <h1>{{ $categories[0]['name'] }}</h1>
                         <p>{{$categories[0]['description']}}</p>
-                        <a href="{{ route('shop.index', ['category' => $categories[0]['slug']]) }}">Shop now</a>
+                        <a href="{{ route('shop.index', ['c' => $categories[0]['slug']]) }}">Shop now</a>
                     </div>
                 </div>
             </div>
@@ -25,11 +24,11 @@
                         
                     <div class="col-lg-6 col-md-6 col-sm-6 p-0">
                         <div class="categories__item set-bg"
-                            data-setbg="{{ asset('themes/ashion//img/categories/category-2.jpg') }}">
+                            data-setbg="{{ productImage($categories[$i]['image']) }}">
                             <div class="categories__text">
                                 <h4>{{ $categories[$i]['name'] }}</h4>
-                                <p>{{ $categories[$i]['product_count'] }}</p>
-                                <a href="{{ route('shop.index', ['category' => $categories[$i]['slug']]) }}">Shop now</a>
+                                <p>{{ $categories[$i]['product_count'] }} items</p>
+                                <a href="{{ route('shop.index', ['c' => $categories[$i]['slug']]) }}">Shop now</a>
                             </div>
                         </div>
                     </div> 
